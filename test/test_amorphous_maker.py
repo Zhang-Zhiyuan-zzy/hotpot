689af3eb63de088ca3943f452e4fe92fadbee321
Yuqing Qiu <2022223070045@stu.scu.edu.cn>
2023-05-04 04:47:12 +0300

    add scan aC maker

--- test/test_amorphous_maker.py
+++ test/test_amorphous_maker.py
@@ -10,7 +10,8 @@ from src.cheminfo import Molecule as Mol
 
 mol = Mol.create_aCryst_by_mq(
     elements={'C': 1.0}, force_field='aMaterials/SiC.tersoff',
-    ff_args=('C',), path_dump_to='/home/zz1/qyq/melt-quench.xyz',
-    fmt='xyz', density=0.8, path_writefile='/home/zz1/qyq/write_mq.xyz'
+    ff_args=('C',), path_dump_to='/home/qyq/proj/lammps/mq_test_random/try4/melt-quench-1.3_1.xyz',
+    fmt='xyz', density=1.3, melt_temp=5000, highest_temp=10000
 )
-mol.writefile('cif', 'path/to/save')
+mol.crystal().space_group = 'P1'
+mol.writefile('cif', '/home/qyq/proj/lammps/mq_test_random/try4/mq-1.3_1.cif')

35d87b56c554038f7666ec957ea1e3498143ca86
Zhiyuan Zhang <ZhiyuanZhang_scu@163.com>
2023-11-14 15:44:45 +0800

    1) create class SHAPlot to easily make plots relating to SHAP analysis; 2) add new methods insert_other_axes_into() to allow other axes to be insert into the a main axes on the field or span of the main axes

--- hotpot/plots.py
+++ hotpot/plots.py
@@ -12,8 +12,11 @@ Notes:
 """
 from typing import *
 import string
+import re
+from copy import copy
 
 import numpy as np
+import shap
 from scipy.stats import gaussian_kde
 from sklearn import linear_model
 from sklearn.feature_selection import r_regression
@@ -32,25 +35,22 @@ class SciPlot:
     _figheight = 8.205  # inch
     _dpi = 300
 
-    _ax_pos = [0.1483, 0.1657, 0.7163, 0.7185]
+    default_axes_pos = [0.1483, 0.1657, 0.7163, 0.7185]
     _font = 'Arial'
     _splinewidth = 3
     _tick_fontsize = 28
     _xy_label_fontsize = 32
 
     _superscript_xy_frac = (0.025, 0.925)
-    _superscript_fontsize = 32
+    superscript_dict = {'font': _font, 'fontsize': 32, "fontweight": 'bold'}
 
-    def __init__(self, plotters: Union[Callable, np.ndarray[Callable]]):
+    def __init__(self, plotters: Union[Callable, np.ndarray[Callable]], superscript: bool = True,
+                 post_process: Callable = None, post_process_kwargs: dict = None):
+        assert isinstance(plotters, np.ndarray) or isinstance(plotters, Callable)
         self.plotters = plotters if isinstance(plotters, np.ndarray) else np.array([[plotters]])
         self.nrows, self.ncols = self.plotters.shape
 
         fig, axs = plt.subplots(self.nrows, self.ncols)
-
-        self.x_dividers = [i * 1 / self.ncols for i in range(self.ncols)]
-        self.y_dividers = list(reversed([i * 1 / self.nrows for i in range(self.nrows)]))
-
-        self.axs_pos = np.zeros([self.nrows, self.ncols, 4])
 
         self.fig = fig
         if self.nrows == self.ncols == 1:
@@ -65,6 +65,11 @@ class SciPlot:
 
         self.fig.set(figwidth=self.figwidth, figheight=self.figheight, dpi=self._dpi)
 
+        self.superscript = superscript
+
+        self.post_process = post_process
+        self.post_process_kwargs = post_process_kwargs if post_process_kwargs else {}
+
     def __call__(self, *args, **kwargs):
         """"""
         for i, (ax, plotter) in enumerate(zip(self.axs.flatten(), self.plotters.flatten())):
@@ -75,16 +80,18 @@ class SciPlot:
             self.set_ticks(ax, ins_ax)
             self.set_axes_position(ax, ins_ax)
 
-            if self.plotters.size > 1:
+            if self.plotters.size > 1 and self.superscript:
                 self.add_superscript(ax, i)
+
+        if self.post_process:
+            self.post_process(self.fig, self.axs, **self.post_process_kwargs)
 
         return self.fig, self.axs
 
     def add_superscript(self, ax, i):
         xfrac, yfrac = self._superscript_xy_frac
-        fontdict = {'font': self._font, 'fontsize': self._superscript_fontsize, "fontweight": 'bold'}
 
-        self.add_text(ax, xfrac, yfrac, string.ascii_lowercase[i], fontdict)
+        self.add_text(ax, xfrac, yfrac, string.ascii_lowercase[i], self.superscript_dict)
 
     @staticmethod
     def add_text(ax, xfrac: float, yfrac: float, s: str, fontdict=None):
@@ -100,23 +107,63 @@ class SciPlot:
         d = gaussian_kde(xy)(xy)
         return np.log2(d)
 
-    def set_axes_position(self, main_ax: plt.Axes, ins_ax: plt.Axes = None):
-        if self.nrows == self.ncols == 1:
-            pos = self._ax_pos
-        else:
-            i, j = map(int, np.where(self.axs == main_ax))
+    @staticmethod
+    def calc_active_span(nrows, ncols, i, j):
+        """ calculate the active span of Axes, according to the arranging of all Axes
+        and the position of calculated Axes """
+        x_dividers = [i * 1 / ncols for i in range(ncols)]
+        y_dividers = list(reversed([i * 1 / nrows for i in range(nrows)]))
 
-            left = self.x_dividers[j] + 1 / self.ncols * self._ax_pos[0]
-            bottom = self.y_dividers[i] + 1 / self.nrows * self._ax_pos[1]
-            width = 1 / self.ncols * self._ax_pos[2]
-            height = 1 / self.nrows * self._ax_pos[3]
+        return x_dividers[j], y_dividers[i]
 
-            pos = [left, bottom, width, height]
+    @staticmethod
+    def calc_subaxes_pos(ax: plt.Axes, left: float, bottom: float, width: float, height: float):
+        """
+        Calculate the position of the image on the entire canvas based on its span ratio relative to its active area
+        Args:
+            ax: the Axes whose position is calculated
+            left: the fraction span from the left active boundary to the left spline
+            bottom: the fraction span from the bottom active boundary to the bottom spline
+            width: the fraction span from the left spline to the right spline
+            height: the fraction span from the bottom spline to the top spline
 
-        main_ax.set_position(pos)
+        Returns:
+            the (left, bottom, width, height) of calculated Axes relate to the entire canvas.
+        """
+        # Retrieve the axes grid position in the subplot
+        subplotspec = ax.get_subplotspec().get_topmost_subplotspec()
+        i, j = subplotspec.rowspan[0], subplotspec.colspan[0]
+        nrows, ncols = subplotspec.get_gridspec().nrows, subplotspec.get_gridspec().ncols
+        # i, j = map(int, np.where(self.axs == ax))
+        left_boundary, bottom_boundary = SciPlot.calc_active_span(nrows, ncols, i, j)
 
-        if ins_ax:
-            ins_ax.set_position(pos)
+        left = left_boundary + 1 / ncols * left
+        bottom = bottom_boundary + 1 / nrows * bottom
+        width = 1 / ncols * width
+        height = 1 / nrows * height
+
+        return left, bottom, width, height
+
+    @classmethod
+    def insert_other_axes_into(cls, main_ax: plt.Axes, insert_ax: plt.Axes, relative_pos: Union[tuple, np.ndarray]):
+        """
+        insert other Axes into the main Axes, given the given position relating to span of main Axes as the base.
+        Args:
+            main_ax: where the other Axes is inserted into
+            insert_ax: the inserted Axes
+            relative_pos: the relative position for the main Axes that the inserted Axes places. all values of
+             the relative_pos is from 0 to 1.
+        """
+        cls.set_axes_position(insert_ax, pos=cls.calc_subaxes_pos(main_ax, *relative_pos))
+
+    @classmethod
+    def set_axes_position(cls, *axs, pos: Optional[Union[np.ndarray, Sequence]] = None):
+        if not pos:
+            pos = cls.calc_subaxes_pos(axs[0], *cls.default_axes_pos)
+
+        for ax in axs:
+            if isinstance(ax, plt.Axes):
+                ax.set_position(pos)
 
     def set_xylabel_font(self, main_ax: plt.Axes, ins_ax: plt.Axes = None):
         """"""
@@ -303,4 +350,118 @@ class Pearson(PlotTemplet):
         ax.set_xlabel(self.xlabel)
         ax.set_ylabel(self.ylabel)
 
-        SciPlot.add_text(ax, 0.025, 0.850, f"Pearson R={round(coeff, 3)}", {'fontsize': 20})
+        SciPlot.add_text(ax, 0.125, 0.900, f"Pearson R={round(coeff, 3)}", {'fontsize': 48})
+
+
+class SHAPlot(PlotTemplet):
+    """ this class is used to make plots for analyzing the SHAP results """
+    _plot_type = Literal['bar', 'beeswarm', 'scatter', 'waterfull']
+
+    def __init__(self, exp: shap.Explanation, plot_type: _plot_type = 'bar', *args, **kwargs):
+        """"""
+        self.exp = exp
+        self.plot_type = plot_type
+        self.args = args
+        self.kwargs = kwargs
+
+    def __call__(self, ax: plt.Axes):
+        """"""
+        plt.sca(ax)  # Set current Axes to the given
+
+        fig = plt.gcf()
+
+        fig_params = {'figwidth': fig.get_figwidth(), 'figheight': fig.get_figheight(), 'dpi':  fig.get_dpi()}
+
+        if self.plot_type == 'bar':
+            self._bar(ax)
+
+        elif self.plot_type == 'beeswarm':
+            self._beeswarm(ax)
+
+        fig.set(**fig_params)  # recover the attributes of whole figure.
+
+    def _bar(self, ax: plt.Axes):
+        """ make the SHAP bar plot """
+        shap.plots.bar(self.exp, *self.args, **self.kwargs, show=False)
+
+        for bar_value_text in ax.texts:
+            bar_value_text.set(font='Arial', fontsize=20, fontweight='bold')
+
+    def _beeswarm(self, ax: plt.Axes):
+        """ make the SHAP beeswarm plot """
+        shap.plots.beeswarm(self.exp, *self.args, **self.kwargs, show=False)
+
+        # Setting the colorbar
+        fig = ax.figure
+        colorbar = [a for a in fig.get_axes() if a.get_label() == '<colorbar>'][0]
+
+        # adjust the position for colorbar
+        pos = copy(SciPlot.default_axes_pos)
+        pos[0], pos[2] = 0.90, 0.05
+        SciPlot.insert_other_axes_into(ax, colorbar, pos)
+
+        # adjust the colorbar labels
+        colorbar.set_ylabel('Feature Value (Normalized)',
+                            fontdict={'font': "Arial", 'fontsize': 20, 'fontweight': 'bold'})
+
+        # adjust the ticklabel of colorbar
+        for ticklabel in colorbar.get_yticklabels():
+            ticklabel.set(font='Arial', fontsize=16)
+
+    @staticmethod
+    def add_superscript(fig: plt.Figure, axs: np.ndarray):
+        def autowrap(text: str, max_len=12):
+            """ autowrap when the label is too long """
+            new_label = ''
+            line_len = 0
+            for s in text.split():
+                if len(s) > max_len and line_len:
+                    new_label = f'\n{s}\n'
+                    line_len = 0
+
+                elif line_len + len(s) > max_len:
+                    new_label += f'\n{s}'
+                    line_len = len(s)
+
+                elif line_len:
+                    new_label += f' {s}'
+                    line_len += len(s) + 1
+                else:
+                    new_label += s
+                    line_len = len(s)
+
+            return new_label
+
+        marcher = re.compile(r'Sum of \d+ other features')
+
+        x_frac, y_frac = -0.05, 1.05  # the superscript of Axes
+        for i, ax in enumerate(axs.flatten()):
+            SciPlot.add_text(ax, x_frac, y_frac, string.ascii_lowercase[i], SciPlot.superscript_dict)
+
+            if ax.get_xlabel() == 'SHAP value (impact on model output)':
+                ax.set_xlabel('SHAP value', fontdict={'fontsize': 28})
+            else:
+                ax.set_xlabel(ax.get_xlabel(), fontdict={'fontsize': 28})
+
+            ticks = []
+            for tick in ax.xaxis.get_ticklabels():
+                if marcher.fullmatch(tick.get_text()):
+                    tick.set(text=f'other {tick.get_text().split()[2]}', fontsize=22)
+                else:
+                    tick.set(text=autowrap(tick.get_text()), fontsize=22)
+                ticks.append(tick)
+            ax.set_xticklabels(ticks)
+
+            ticks = []
+            for tick in ax.yaxis.get_ticklabels():
+                if marcher.fullmatch(tick.get_text()):
+                    tick.set(text=f'other {tick.get_text().split()[2]}', fontsize=22)
+                else:
+                    tick.set(text=autowrap(tick.get_text()), fontsize=22)
+
+                ticks.append(tick)
+            ax.set_yticklabels(ticks)
+
+
+
+

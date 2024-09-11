414ff4f9891153d5c7dc3922211295337870647c
Zhiyuan Zhang <zhiyuanzhang_scu@163.com>
2024-09-11 11:31:40 +0800

    1) add hotpot/work module; 2) add hotpot/plugin/pubchem module

--- hotpot/works/__init__.py
+++ hotpot/works/__init__.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e5613a8907f8452c53ad422b4893b0d0c35ca6e6bd292d811b511af74017fd9f
-size 119
+oid sha256:729b22f685b2175249cb2b6411357bc6050fe61d1697bd44a451d847e6e766b0
+size 128
--- hotpot/works/format_convert.py
+++ hotpot/works/format_convert.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5fa46aaef9e30761aaf8977fd7bae51b12baba36cf26426eb30768d9067a7763
-size 1519
+oid sha256:7f6c5c36fc4b8eb13c7ad02cb6d22a3bad0bf774c5179fcdea14cdcb5f28f9c9
+size 1572

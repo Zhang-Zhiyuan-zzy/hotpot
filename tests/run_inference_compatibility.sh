86e6eac384f6632208da487cb1ab7fc7d8ff5ef7
Zhiyuan Zhang <841651891@qq.com>
2026-09-16 19:52:01 +0800

    test(ci): cover forcefield and conversion workflows

--- tests/run_inference_compatibility.sh
+++ tests/run_inference_compatibility.sh
@@ -22,9 +22,18 @@ for version in "${versions[@]}"; do
             tests/test_cheminfo/test_ob2chem_compat.py \
             tests/test_cheminfo/test_bond_metadata.py \
             tests/test_cheminfo/test_ligand_rings.py \
+            tests/test_cheminfo/test_geometry.py \
+            tests/test_cheminfo/test_geometry_quality.py \
+            tests/test_cheminfo/test_forcefield_api.py \
+            tests/test_cheminfo/test_forcefield_optimizer.py \
+            tests/test_cheminfo/test_forcefield_integration.py \
+            tests/test_cheminfo/test_complexes_build.py \
+            tests/test_cheminfo/test_complex_hydrogens.py \
+            tests/test_cheminfo/test_topology_determinism.py \
             tests/test_cheminfo/test_search.py \
             tests/test_cheminfo/test_search_mapping.py \
             tests/test_cheminfo/test_smarts.py \
+            tests/test_works/test_convert.py \
             tests/test_cheminfo/test_import_safety.py \
             tests/test_smart_parser.py
 

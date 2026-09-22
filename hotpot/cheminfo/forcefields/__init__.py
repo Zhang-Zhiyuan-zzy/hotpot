ba67c9188c05860455daf5b8d1df46d255cfc4ab
Zhiyuan Zhang <841651891@qq.com>
2026-09-22 18:59:20 +0800

    style(forcefields): mark selected facade exports

--- hotpot/cheminfo/forcefields/__init__.py
+++ hotpot/cheminfo/forcefields/__init__.py
@@ -4,8 +4,8 @@ import sys
 
 
 if sys.version_info[:2] == (3, 9):
-    from .ff39 import *
-    from .ff39 import __all__
+    from .ff39 import *  # noqa: F403
+    from .ff39 import __all__ as __all__
 else:
-    from .ff import *
-    from .ff import __all__
+    from .ff import *  # noqa: F403
+    from .ff import __all__ as __all__
